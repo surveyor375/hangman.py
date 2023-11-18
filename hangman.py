e7102e6556f9214b6c11c2e883c2f1fe46079171
Mateusz Kaczmarek <surtech375@gmail.com>
2023-11-18 19:24:22 +0100

    first commit

--- hangman.py
+++ hangman.py
@@ -5,18 +5,39 @@ print("Welcome to hangman!")
 
 # create your wordlist
 words = ["hacker", "bounty", "random"]
-
 # randomly choose a word from the list you have created
 secret_word = random.choice(words)
+print(secret_word)
+print("You have 5 guesses")
+display_word = []
 
+for lett in secret_word:
+    display_word += "_"
+print(display_word)
 # ask the user to guess a letter
-guess = input("Guess a letter").lower()
 
 # bonus make the program take input from the user and make it lowercase
 
 # check if the letter is in the word
-for letter in secret_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+num = 0
+game_over = False
+while not game_over:
+    guess = input("Guess a letter").lower()
+
+    for position in range(len(secret_word)):
+        letter = secret_word[position]
+        if letter == guess:
+            display_word[position] = letter
+    if guess not in secret_word:
+        num+=1
+        guesses_left = 5 - num
+        print(f"You have {guesses_left}")
+        if num>= 5:
+            print("You lost")
+            game_over = True    
+
+    print(display_word)
+
+    if "_" not in display_word:
+        print("you won")
+        game_over = True 
